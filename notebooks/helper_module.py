b1e70007fa59c342bd0ec2db484dab95f6a66a65
Andre Weiner <andre.weiner@yahoo.de>
2019-09-17 23:09:44 +0200

    Save progress on refactoring.

--- notebooks/helper_module.py
+++ notebooks/helper_module.py
@@ -7,6 +7,7 @@ Cotains the following data structures:
 
 import pandas as pd
 import numpy as np
+import matplotlib.tri as tri
 
 # parameters for uniform plot appearance
 alpha_contour = 0.75
@@ -19,6 +20,8 @@ line_width = 3
 
 
 class Logfile():
+    """Load and evaluate simulation log-files."""
+
     def __init__(self, path=None):
         """Initialize a Logfile object.
 
@@ -119,3 +122,84 @@ class Logfile():
         """
         row = self.log.iloc[(self.log[value_name] - value).abs().argsort()[:1]]
         return row
+
+
+class CenterFieldValues2D():
+    def __init__(self, path=None, center=None, u_b=None):
+        """Initialize CenterFieldValue2D object.
+
+        Parameters
+        ----------
+        path - String : path to data file
+        center - array-like : [x,y] coordinates of the center of mass
+        U_b - array-like : [u_x, u_y] components of the bubble rise velocity
+
+        """
+        self.path = path
+        self.center = center
+        self.u_b = u_b
+        self.read_field()
+        self.create_triangulation()
+
+    def read_field(self):
+        """Read center values from disk."""
+        try:
+            names = ['f', 'ref', 'u_x', 'u_y', 'u_z', 'x', 'y', 'z']
+            usecols = ['f', 'u_x', 'u_y', 'x', 'y']
+            self.data = pd.read_csv(self.path, sep=',', header=0, names=names, usecols=usecols)
+            print("Successfully read file \033[1m{}\033[0m".format(self.path))
+        except Exception as read_exc:
+            print("Error reading data from disk for file \033[1m{}\033[0m".format(self.path))
+            print(str(read_exc))
+
+    def create_triangulation(self):
+        """Create a triangulation from points."""
+        self.triang = tri.Triangulation(
+            self.data['x'].values-self.center[0],
+            self.data['y'].values-self.center[1])
+
+    def interpolate_velocity(self, xi, yi, relative=False, magnitude=True):
+        """Interpolate velocity at given points.
+
+        Parameters
+        ----------
+        xi, yi - array-like : x and y coordinates of interpolation points
+        relative - Boolean : compute velocity relative to u_b if True
+        magnitude - Boolean : return magnitude of vector if True
+
+        Return
+        ------
+        u_xi, u_yi - array-like : interpolated velocity components
+        mag(u_i) - array-like : magnitude of interpolated velocity
+
+        """
+        interpolator_u_x = tri.CubicTriInterpolator(
+            self.triang, self.data['u_x'].values, kind='geom')
+        interpolator_u_y = tri.CubicTriInterpolator(
+            self.triang, self.data['u_y'].values, kind='geom')
+        u_xi = interpolator_u_x(xi, yi)
+        u_yi = interpolator_u_y(xi, yi)
+        if relative:
+            u_xi -= self.u_b[0]
+            u_yi -= self.u_b[1]
+
+        if magnitude:
+            return np.sqrt(np.square(u_xi) + np.square(u_yi))
+        else:
+            return u_yi, u_xi  # paraview bug: the transform filter does not swap the vector components
+
+    def interpolate_volume_fraction(self, xi, yi):
+        """Interpolate volume fraction at given points.
+
+        Parameters
+        ----------
+        xi, yi - array-like : x and y coordinates of interpolation points
+
+        Return
+        ------
+        f - array-like : interpolated volume fraction
+
+        """
+        self.interpolator_f = tri.CubicTriInterpolator(
+            self.triang, self.data['f'].values, kind='geom')
+        return self.interpolator_f(xi, yi)
